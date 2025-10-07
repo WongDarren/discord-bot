963702637966440a96db27a2b6824b36a268ee36
Darren Wong <wongdarren1997@gmail.com>
2025-10-06 19:18:19 -0500

    add HikariCP configuration and clean up main method in DiscordBotApplication

--- src/main/java/com/wongdarren/discordbot/DiscordBotApplication.java
+++ src/main/java/com/wongdarren/discordbot/DiscordBotApplication.java
@@ -7,9 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class DiscordBotApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DiscordBotApplication.class, args);
-
-
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DiscordBotApplication.class, args);
+  }
 }
